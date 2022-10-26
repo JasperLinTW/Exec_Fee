17e01b20261fa5c3dabff8e3937afb9b1ea0108c
Jasper <guitar1859@gmail.com>
2022-10-26 10:29:58 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maleRadioButton = new System.Windows.Forms.RadioButton();
+            this.FemaleRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.FeeLable = new System.Windows.Forms.Label();
+            this.reasonLable = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "年齡:";
+            // 
+            // ageTxtBox
+            // 
+            this.ageTxtBox.Location = new System.Drawing.Point(50, 19);
+            this.ageTxtBox.Name = "ageTxtBox";
+            this.ageTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.ageTxtBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "性別: ";
+            // 
+            // maleRadioButton
+            // 
+            this.maleRadioButton.AutoSize = true;
+            this.maleRadioButton.Location = new System.Drawing.Point(197, 22);
+            this.maleRadioButton.Name = "maleRadioButton";
+            this.maleRadioButton.Size = new System.Drawing.Size(35, 16);
+            this.maleRadioButton.TabIndex = 3;
+            this.maleRadioButton.TabStop = true;
+            this.maleRadioButton.Tag = "1";
+            this.maleRadioButton.Text = "男";
+            this.maleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // FemaleRadioButton
+            // 
+            this.FemaleRadioButton.AutoSize = true;
+            this.FemaleRadioButton.Location = new System.Drawing.Point(238, 22);
+            this.FemaleRadioButton.Name = "FemaleRadioButton";
+            this.FemaleRadioButton.Size = new System.Drawing.Size(35, 16);
+            this.FemaleRadioButton.TabIndex = 4;
+            this.FemaleRadioButton.TabStop = true;
+            this.FemaleRadioButton.Tag = "0";
+            this.FemaleRadioButton.Text = "女";
+            this.FemaleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FemaleRadioButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.maleRadioButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ageTxtBox);
+            this.groupBox1.Controls.Add(this.CalcButton);
+            this.groupBox1.Location = new System.Drawing.Point(36, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(305, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "客戶資訊";
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Location = new System.Drawing.Point(50, 46);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(75, 23);
+            this.CalcButton.TabIndex = 5;
+            this.CalcButton.Text = "計算";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // FeeLable
+            // 
+            this.FeeLable.AutoSize = true;
+            this.FeeLable.Location = new System.Drawing.Point(15, 28);
+            this.FeeLable.Name = "FeeLable";
+            this.FeeLable.Size = new System.Drawing.Size(35, 12);
+            this.FeeLable.TabIndex = 0;
+            this.FeeLable.Text = "車資: ";
+            // 
+            // reasonLable
+            // 
+            this.reasonLable.AutoSize = true;
+            this.reasonLable.Location = new System.Drawing.Point(15, 52);
+            this.reasonLable.Name = "reasonLable";
+            this.reasonLable.Size = new System.Drawing.Size(35, 12);
+            this.reasonLable.TabIndex = 1;
+            this.reasonLable.Text = "原因: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.FeeLable);
+            this.groupBox2.Controls.Add(this.reasonLable);
+            this.groupBox2.Location = new System.Drawing.Point(36, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(305, 82);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "計算結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 242);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ageTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton maleRadioButton;
+        private System.Windows.Forms.RadioButton FemaleRadioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Label FeeLable;
+        private System.Windows.Forms.Label reasonLable;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{/// <summary>
+/// 根據客戶性別及年齡計算應付車資, 規則如下:
+/// - <=3 零元
+/// - >=70, 男, 2元
+/// - >=60, 女, 3元
+/// - 全票, 15元
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            int age;
+            try
+            {
+                age = GetAge();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            bool gender;
+            try
+            {
+                gender = GetGender();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            string[] result = Calculate(age , gender);
+
+            FeeLable.Text = $"車資: {result[0]}元";
+            reasonLable.Text = $"原因: {result[1]}";
+        }
+
+        private string[] Calculate(int age, bool gender)
+        {
+            int fee;
+            string reason = string.Empty;
+            if (age <= 3){fee = 0; reason = "年齡小於等於3歲"; }
+            else if (age >= 70 && gender) { fee = 2; reason ="年齡大於七十歲男性"; }
+            else if (age >= 60 && !gender ) { fee = 3; reason = "年齡大於六十歲女性"; }
+            else { fee = 15; reason = "您屬於全票範圍"; }
+            string feeResult = fee.ToString();
+            string[] result = new string[] {feeResult, reason};
+            return result;
+            
+        }
+
+        private bool GetGender()
+        {
+            if (maleRadioButton.Checked) { return true; }
+            else if(FemaleRadioButton.Checked) { return false; }
+            else { throw new Exception("您未選取性別"); }
+        }
+
+        private int GetAge()
+        {
+            string input = ageTxtBox.Text;
+            bool isInt = int.TryParse(input, out int age);
+            return isInt ? age : throw new Exception("您輸入的年齡格式不正確");
+
+        }
+    }
+}
